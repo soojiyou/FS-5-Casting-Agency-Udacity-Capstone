b7dc96e09c84ce713b3084aaa4d8d12752045bf9
syou <dbtnwl369@gmail.com>
2023-03-14 00:32:54 -0700

    First oommit for FSND model draft, endpoint setting draft

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,342 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import Actors, Movies, db_drop_and_create_all, setup_db
+import dateutil.parser
+import babel
+
+from auth.auth import AuthError, requires_auth
+# from config import app
+
+
+# app = Flask(__name__)
+# setup_db(app)
+# CORS(app)
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+    db_drop_and_create_all()
 
-  return app
+    # CORS Headers
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers',
+            'Content-Type,Authorization,true')
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+    '''@Todo endpoint get /actors and /movies
+    /actors : get a list of actors
+    /movies : get a list of movies
+
+'''
+
+    @app.route('/actors', methods=['GET'])
+    # @requires_auth('get:actor')
+    def get_actors():
+        actors_list = {}
+        actors = Actors.query.all()
+        if not actors:
+            return abort(404)
+
+        for actor in actors:
+            actors_list[actor.id] = actor.name
+
+        return jsonify({
+            "success": True,
+            "actors": actors_list,
+        }), 200
+
+    @app.route('/movies', methods=['GET'])
+    # @requires_auth('get:movie')
+    def get_movies():
+        movies_list = {}
+        movies = Movies.query.all()
+        if not movies:
+            return abort(404)
+
+        for movie in movies:
+            movies_list[movie.id] = movie.title
+
+        return jsonify({
+            "success": True,
+            "actors": movies_list,
+        }), 200
+
+    '''@Todo endpoint get /actor and /movie
+        /actors/<int:actor_id>' : get a detail of selected actor
+        /movies/<int:movie_id> : get a detail of selected movie
+
+    '''
+
+    @app.route('/actors/<int:actor_id>', methods=['GET'])
+    @requires_auth('get:actor-detail')
+    def get_actor_detail(actor_id):
+        actor_data = {}
+        actor_detail = Actors.query.get(actor_id)
+        if not actor_detail:
+            return abort(404)
+
+        actor_data.append(actor_detail.format())
+
+        return jsonify({
+            "success": True,
+            "actor_detal": actor_data,
+        }), 200
+
+    @app.route('/movies/<int:movie_id>', methods=['GET'])
+    @requires_auth('get:movie-detail')
+    def get_movie_detail(movie_id):
+        movie_data = {}
+        movie_detail = Actors.query.get(movie_id)
+        if not movie_detail:
+            return abort(404)
+
+        movie_data.append(movie_detail.format())
+
+        return jsonify({
+            "success": True,
+            "actor_detal": movie_data,
+        }), 200
+
+    '''@Todo endpoint delete target /actor and /movie
+        /actors/<int:actor_id>' : delete selected actor
+        /movies/<int:movie_id> : delete selected movie
+
+    '''
+
+    @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+    @requires_auth('delete:actor')
+    def delete_actor(payload, actor_id):
+        target_actor = Actors.query.get(actor_id)
+        if not target_actor:
+            abort(404)
+
+        try:
+            target_actor.delete()
+
+            return jsonify({
+                'success': True,
+                'delete': actor_id,
+            }), 200
+
+        except Exception:
+            abort(422)
+
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    @requires_auth('delete:movie')
+    def delete_movie(payload, movie_id):
+        target_movie = Actors.query.get(movie_id)
+        if not target_movie:
+            abort(404)
+
+        try:
+            target_movie.delete()
+
+            return jsonify({
+                'success': True,
+                'delete': movie_id,
+            }), 200
+
+        except Exception:
+            abort(422)
+
+    '''@Todo endpoint post new /actor and /movie
+        /actors : post new actor
+        /movies : post new movie
+
+    '''
+
+    @app.route('/actors', methods=['POST'])
+    @requires_auth('post:actor')
+    def post_actors(payload):
+        new_name = request.json.get('name')
+        new_age = request.json.get('age')
+        new_gender = request.json.get('gender')
+        new_image = request.json.get('image_link')
+
+        if not new_name and new_age and new_gender and new_image:
+            abort(400)
+        try:
+            post_new_actor = Actors(
+                name=new_name, age=new_age, gender=new_gender, image_link=new_image)
+            post_new_actor.insert()
+
+            actor_profile = post_new_actor.format()
+            return jsonify({
+                "success": True,
+                "posted_actor_profile": [actor_profile],
+            }), 200
+        except:
+            abort(422)
+
+    @app.route('/movies', methods=['POST'])
+    @requires_auth('post:movie')
+    def post_movies(payload):
+        new_title = request.json.get('name')
+        new_release_date = request.json.get('age')
+        new_genres = request.json.get('gender')
+        new_image = request.json.get('image_link')
+
+        if not new_title and new_release_date and new_genres and new_image:
+            abort(400)
+        try:
+            post_new_movie = Movies(title=new_title, release_date=new_release_date,
+                                    genres=new_genres, image_link=new_image)
+            post_new_movie.insert()
+
+            movie_profile = post_new_movie.format()
+            return jsonify({
+                "success": True,
+                "posted_movie_profile": [movie_profile],
+            }), 200
+        except:
+            abort(422)
+
+    '''@Todo endpoint patch requested /actor and /movie
+        /actors/<int:actor_id> : patch requested actor
+        /movies/<int:movie_id> : patch requested movie
+
+    '''
+
+    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+    @requires_auth('patch:actor')
+    def update_actors(pqyload, actor_id):
+        requested_actor = Actors.query.get(actor_id)
+        if not requested_actor:
+            abort(404)
+
+        req_name = request.json.get('name')
+        req_age = request.json.get('age')
+        req_gender = request.json.get('gender')
+        req_image = request.json.get('image_link')
+
+        if not req_name or not req_age or not req_gender or not req_image:
+            abort(400)
+
+        try:
+            if req_name:
+                requested_actor.name = req_name
+
+            if req_age:
+                requested_actor.age = req_age
+
+            if req_gender:
+                requested_actor.gender = req_gender
+
+            if req_image:
+                requested_actor.image_link = req_image
+
+            requested_actor.update()
+
+            patched_actor = requested_actor.format()
+            return jsonify({
+                'success': True,
+                'patched_actor_profile': [patched_actor],
+            }), 200
+
+        except Exception:
+            abort(422)
+
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    @requires_auth('patch:movie')
+    def update_movies(pqyload, movie_id):
+        requested_movie = Movies.query.get(movie_id)
+        if not requested_movie:
+            abort(404)
+
+        req_title = request.json.get('name')
+        req_release_date = request.json.get('age')
+        req_genres = request.json.get('gender')
+        req_image = request.json.get('image_link')
+
+        if not req_title or not req_release_date or not req_genres or not req_image:
+            abort(400)
+
+        try:
+            if req_title:
+                requested_movie.title = req_title
+
+            if req_release_date:
+                requested_movie.release_date = req_release_date
+
+            if req_genres:
+                requested_movie.genres = req_genres
+
+            if req_image:
+                requested_movie.image_link = req_image
+
+            requested_movie.update()
+
+            patched_movie = requested_movie.format()
+            return jsonify({
+                'success': True,
+                'patched_movie_profile': [patched_movie],
+            }), 200
+
+        except Exception:
+            abort(422)
+
+
+# Error Handling
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"success": False,
+                        "error": 404,
+                        "message": "Not found"}), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False,
+                    "error": 422,
+                     "message": "unprocessable"}), 422
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False,
+                        "error": 400,
+                        "message": "bad request"}), 400
+
+    @app.errorhandler(AuthError)
+    def not_authenticated(auth_error):
+        return jsonify({
+            "success": False,
+            "error": auth_error.status_code,
+            "message": auth_error.error
+        }), 401
+    return app
+
 
 APP = create_app()
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
+
+
+# def format_datetime(value, format='medium'):
+
+#     if isinstance(value, str):
+#         date = dateutil.parser.parse(value)
+#     else:
+#         date = value
+#     return babel.dates.format_datetime(date, format, locale='en')
+
+
+# APP.jinja_env.filters['datetime'] = format_datetime
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,137 @@
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort, jsonify
+# from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+# from forms import *
+from flask_migrate import Migrate
+# from app import APP
+from sqlalchemy import Column, String, Integer, create_engine
+# from datetime import datetime
+import os
+# ----------------------------------------------------------------------------#
+# App Config.
+# ----------------------------------------------------------------------------#
 
+# app = Flask(__name__)
+# moment = Moment(app)
+# app.config.from_object('config')
+# db = SQLAlchemy(app)
+
+# # TODO: connect to a local postgresql database
+# migrate = Migrate(app, db)
+
+# # with app.app_context():
+#     db.create_all()
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+
+# db = SQLAlchemy(APP)
+# migrate = Migrate(APP, db)
+
+
+database_name = "capstone"
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+# database_path = "postgres:///{}".format(database_name)
+# database_path = os.environ['DATABASE_URL']
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_name))
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+        binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    # add one demo row which is helping in POSTMAN test
+    movie = Movies(
+        title='movietest1',
+        release_date='20230315',
+        genres='sci-fi',
+
+        image_link='https://www.bing.com/th?id=OIP.M9AsZ7Sm6Qq-LXpY92Tt2AHaEK&w=180&h=185&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2'
+    )
+
+    movie.insert()
+
+
+class Movies(db.Model):
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    # release_date = db.Column(db.DateTime, nullable=False)
+    release_date = db.Column(db.String, nullable=False)
+    genres = db.Column(db.String, nullable=False)
+
+    image_link = db.Column(db.String(500))
+
+    def __init__(self, title, release_date, genres, image_link):
+        self.title = title
+        self.release_date = release_date
+        self.genres = genres
+        self.image_link = image_link
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        movie_obj = {'movie_id': self.id, 'movie_title': self.title,
+                     'release_date': self.release_date, 'genres': self.genres, 'image_link': self.image_link}
+        return f'<{movie_obj}>'
+
+
+class Actors(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
+
+    image_link = db.Column(db.String(500))
+
+    def __init__(self, name, age, gender, image_link):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.image_link = image_link
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        actor_obj = {'actor_id': self.id, 'actor_name': self.name,
+                     'actor_age': self.age, 'actor_gender': self.gender, 'image_link': self.image_link}
+        return f'<{actor_obj}>'
