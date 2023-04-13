bb6886ee1954cdadbef4c192275d71ebf0875695
syou <dbtnwl369@gmail.com>
2023-04-13 11:44:09 -0700

    change for deploy

--- backend/models.py
+++ backend/models.py
@@ -48,7 +48,8 @@ setup_db(app)
 
 def setup_db(app, database_path=database_path):
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    # app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
